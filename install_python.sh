fd9150846a00b4c9c7c0c7abc7cf0848fba32439
AtkLordOverAll <darkninjabuni@gmail.com>
2019-07-31 21:27:02 +0100

    Made dependency install/removal smarter, made Python log to a separate file

--- install_python.sh
+++ install_python.sh
@@ -4,24 +4,28 @@ if [[ $1 != http?(s)://*.tar.* ]]; then
     echo "Invalid URL"
     exit 2
 fi
-echo "Apt update"
-sudo apt-get update > /dev/null
 echo "Install requirements"
-apt list --installed | sed -e "s/\/.*//g" > tmp.txt
+apt list --installed 2>/dev/null | sed -e "s/\/.*//g" > tmp.txt
 INSTALLS=$(grep -vf tmp.txt reqs.txt | tr "\n" " ")
-sudo apt-get install $INSTALLS -y > /dev/null
-sudo apt-mark auto $INSTALLS
+if [ ${#INSTALLS} -gt 1 ]; then
+    echo "Apt update"
+    sudo apt-get update > /dev/null
+    sudo apt-get install $INSTALLS -y # > /dev/null
+    sudo apt-mark auto $INSTALLS
+else
+    echo "You have all the requirements; awesome!"
+fi
 echo "Downloading and extracting Python"
 wget -O Python.tar.xz $1
 tar xf ./Python.tar.xz
 cd Python-*/
 echo "Configuring"
-./configure --enable-optimizations
+./configure --enable-optimizations > python.log 2>&1
 CORECOUNT=$(grep -c ^processor /proc/cpuinfo)
 echo "Building"
-make -j $CORECOUNT
+make -j $CORECOUNT >> python.log 2>&1
 echo "Installing"
-sudo make altinstall -j $CORECOUNT
+sudo make altinstall -j $CORECOUNT >> python.log 2>&1
 echo "Cleaning up"
 cd ..
 rm Python.tar.xz
